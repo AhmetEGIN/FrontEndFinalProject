74c1f02da935a3cc94ad89058fcb1c7ac791b944
EGIN <EGIN@LAPTOP-JUH3P2T8>
2022-08-22 15:41:42 +0300

    22.08.2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html', // ./ aynı klasör anlamına gelir
+  styleUrls: ['./app.component.css'], // bu html in css lerini koyduğumuz yerdir. Birden fazla css varsa virgül ile ayırabiliriz
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = 'EnginDemiroğ';
 }
+
+// datayı yönettiğimiz yer burasıdır
+// Angular da component demek html in datasını kontrol ettiğimiz yerdir
+//3. satırdaki @Component çalışma anında class a anlam yükler. C# taki attribute lar gibi
+//  süslü parantez demek obje demektir. Obje içerisinde özellikleri verebiliriz
+// Bir classın component olduğunu üstündeki deklerasyon ile anlatırız. neyin componenti olduğunu süslü parantezler-
+// -içerisine yazdığımız özellikler ile belirleriz
